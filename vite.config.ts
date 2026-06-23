799557da04f0913c8f3a21537a95330cbde6766b
sai vishwa <saivishwa692@gmail.com>
2026-06-23 06:40:22 +0530

    site map updated in the vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import sitemap from 'vite-plugin-sitemap'
 
 export default defineConfig({
   base: '/',
@@ -10,6 +11,9 @@ export default defineConfig({
     TanStackRouterVite({ autoCodeSplitting: true }),
     react(),
     tailwindcss(),
+    sitemap({
+      hostname: 'https://ksrsolarsolutions.com',
+    }),
   ],
   resolve: {
     alias: {
